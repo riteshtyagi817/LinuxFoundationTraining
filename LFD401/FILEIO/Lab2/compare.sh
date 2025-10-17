5be63ea9713e7baed4f0100fb4832742b2a27860
Ritesh Tyagi <riteshtyagi725@gmail.com>
2025-10-17 07:28:51 +0530

    enhanced the compare script for build and rm

--- LFD401/FILEIO/Lab2/compare.sh
+++ LFD401/FILEIO/Lab2/compare.sh
@@ -3,5 +3,27 @@
 defaultFile=/etc/passwd
 [ $# -gt 0 ] && defaultFile=$1
 
-./read_hex_offset_stdio "defaultFile" > stdio.out
-../
+
+option=$2
+if [ "$option" = "remove" ]
+then
+	rm ./read_hex_offset_stdio
+	rm ../Lab1/read_hex
+	rm ../Lab1/read_hex_offset
+	rm *.out
+elif [ "$option" = "build" ]
+then
+	gcc -O2 -Wall -o ./read_hex_offset_stdio ./read_hex_offset_stdio.c
+	gcc -O2 -Wall -o ../Lab1/read_hex ../Lab1/read_hex.c
+	gcc -O2 -Wall -o ../Lab1/read_hex_offset ../Lab1/read_hex_offset.c
+fi
+if [ "$option" = "build" ]
+then
+	./read_hex_offset_stdio "$defaultFile" > stdio.out
+	../Lab1/read_hex "$defaultFile" > read_hex.out
+	../Lab1/read_hex_offset "$defaultFile" > read_hex_offset.out
+	hexdump -C "$defaultFile" > hexdump.out
+
+	ls -l *out
+	wc *out
+fi
