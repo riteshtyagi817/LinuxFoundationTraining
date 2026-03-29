17259844a1e4b0fe4307eef384670a87c4a66102
Ritesh Tyagi <riteshtyagi725@gmail.com>
2026-03-29 07:27:37 +0530

    added the client

---
+++ LFD401/SocketsClients/inet_client.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<netdb.h>
+#include<arpa/inet.h>
+
+#define MSG_LEN 1024
+#define PORT_NUMBER 7 
+#define DEFAULT_HOST "localhost"
+
+void get_addr_struct(const char *host, struct sockaddr_in *addr){
+
+	int rc;
+	struct addrinfo *res;
+	rc = getaddrinfo(host, NULL, NULL, &res);
+	if(rc != 0){
+		gai_strerror(rc);
+		exit(EXIT_FAILURE);
+	}	
+	memcpy(addr, res->ai_addr, sizeof(struct sockaddr_in));
+	freeaddrinfo(res);	
+
+
+}
+int main(int argc, char *argv[]){
+
+	int sd, msg_len;
+	char message[MSG_LEN];
+	struct sockaddr_in addr;
+	char *host = DEFAULT_HOST;
+	int rc;
+
+	if(argc > 1)
+		host = argv[1];
+	sd = socket(PF_INET, SOCK_STREAM, 0);
+
+	get_addr_struct(host, &addr);
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(PORT_NUMBER);
+
+	rc = connect(sd, (struct sockaddr *)&addr, sizeof(addr));
+	if(rc != 0){
+		perror("connect");
+		exit(1);
+
+	}
+
+
+	msg_len = strlen(fgets(message, MSG_LEN, stdin));
+	write(sd, message, msg_len);
+
+	memset(message, 0, MSG_LEN);
+	msg_len = read(sd, message, MSG_LEN);
+
+	write(STDOUT_FILENO, message, msg_len);
+
+	close(sd);
+	exit(EXIT_SUCCESS);
+
+}
